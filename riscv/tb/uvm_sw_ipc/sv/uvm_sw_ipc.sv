65b3ff8bebe8bed0a61f8458517ff8dc73389ed8
formation5 <cocodrima.35@hotmail.fr>
2022-11-25 10:00:21 +0100

    modify formation

--- riscv/tb/uvm_sw_ipc/sv/uvm_sw_ipc.sv
+++ riscv/tb/uvm_sw_ipc/sv/uvm_sw_ipc.sv
@@ -24,16 +24,19 @@ class uvm_sw_ipc extends uvm_component;
   // uvm_sw_ipc_quit()                            ---|-->   end of simulation
 
   // high-level API
-  extern function void uvm_sw_ipc_gen_event(int event_idx);
+  extern task	       uvm_sw_ipc_gen_event(int event_idx);
   extern task          uvm_sw_ipc_wait_event(int event_idx);
   extern function void uvm_sw_ipc_push_data(input int fifo_idx, input [31:0] data);
   extern function bit  uvm_sw_ipc_pull_data(input int fifo_idx, output [31:0] data);
 
   uvm_tlm_analysis_fifo#(uvm_sw_ipc_tx) monitor_fifo;
+  uvm_sw_ipc_tx 			ipc_tx;
   uvm_event                             event_to_uvm[UVM_SW_IPC_EVENT_NB];
   uvm_event                             event_to_sw[UVM_SW_IPC_EVENT_NB];
   bit [31:0]                            fifo_data_to_uvm[UVM_SW_IPC_FIFO_NB][$];
   bit [31:0]                            fifo_data_to_sw[UVM_SW_IPC_FIFO_NB][$];
+  bit [31:0]                            fifo_data_push_to_uvm[UVM_SW_IPC_FIFO_NB][$];
+  bit [31:0]				fifo_data_to_sw_empty = 32'hFFFFFFFF;
 
   uvm_sw_ipc_config       m_config;
   uvm_sw_ipc_monitor      m_monitor;
@@ -50,6 +53,7 @@ class uvm_sw_ipc extends uvm_component;
   extern function string str_replace(string str, string pattern, string replacement);
   extern function string str_format(string str, ref bit [31:0] q[$]);
   extern function string str_format_one_arg(string str, bit [31:0] arg, bit fmt_is_string);
+  extern task		 print_info(bit [31:0] cmd);
 endclass : uvm_sw_ipc
 
 
@@ -88,11 +92,74 @@ endfunction : connect_phase
 task uvm_sw_ipc::run_phase(uvm_phase phase);
   phase.raise_objection(this);
   // TODO: proccess monitor_fifo
-  phase.drop_objection(this);
+	ipc_tx = uvm_sw_ipc_tx::type_id::create("ipc_tx",this);
+	fork
+	forever begin
+		#1ns
+		if (m_quit == 1)
+			phase.drop_objection(this);
+	end
+	forever begin
+		@(fifo_data_to_sw_empty);
+		vif.backdoor_write(m_config.fifo_data_to_sw_empty_address,fifo_data_to_sw_empty);
+	end
+	forever begin
+		monitor_fifo.get(ipc_tx);
+		// `uvm_info("debug info",$sformatf("%s",ipc_tx.convert2string()),UVM_LOW)
+		if(ipc_tx.addr inside {[m_config.cmd_address:m_config.fifo_data_to_sw_empty_address]})begin
+		case(ipc_tx.addr) inside
+			m_config.cmd_address: begin
+				case(ipc_tx.data) inside
+					32'h1:  m_quit = 1;
+					32'h2: begin
+						bit [31:0] event_num = fifo_data_to_uvm[0].pop_front();
+						if(event_num<UVM_SW_IPC_EVENT_NB)
+							event_to_uvm[event_num].trigger(); 
+						else
+							event_to_sw[event_num-UVM_SW_IPC_EVENT_NB].trigger();
+					end
+					32'h4: begin//put data in another fifo
+						bit [31:0] fifo_num = fifo_data_to_uvm[0].pop_front();
+						fifo_data_push_to_uvm[fifo_num].push_back(fifo_data_to_uvm[fifo_num].pop_front());
+					end
+					32'h5: begin
+						int fifo_idx = fifo_data_to_uvm[0].pop_front();
+						vif.backdoor_write(m_config.fifo_data_to_sw_address[fifo_idx], fifo_data_to_sw[fifo_idx].pop_front());
+						if(fifo_data_to_sw[fifo_idx].size()==0)
+							fifo_data_to_sw_empty = fifo_data_to_sw_empty | (1<<fifo_idx);
+					end
+					[32'h6:32'h9] : print_info(ipc_tx.data);
+				endcase
+			end
+			[m_config.fifo_data_to_uvm_address[0]:m_config.fifo_data_to_uvm_address[UVM_SW_IPC_FIFO_NB-1]]:begin
+				fifo_data_to_uvm[(ipc_tx.addr-m_config.fifo_data_to_uvm_address[0])/4].push_back(ipc_tx.data);
+			end
+			m_config.fifo_data_to_sw_empty_address:
+				fifo_data_to_sw_empty = ipc_tx.data;
+		endcase
+		end
+	end
+	join_any
 endtask : run_phase
 
+task uvm_sw_ipc::uvm_sw_ipc_gen_event(int event_idx);
+	event_to_sw[event_idx].wait_trigger();
+	vif.backdoor_write(m_config.fifo_data_to_sw_address[0],event_idx);
+	vif.backdoor_write(m_config.cmd_address,3);
+endtask : uvm_sw_ipc_gen_event
 
+task          uvm_sw_ipc::uvm_sw_ipc_wait_event(int event_idx);
+	event_to_uvm[event_idx].wait_trigger();
+endtask : uvm_sw_ipc_wait_event
 
+function void uvm_sw_ipc::uvm_sw_ipc_push_data(input int fifo_idx, input [31:0] data);
+	fifo_data_to_sw[fifo_idx].push_back(data);
+	fifo_data_to_sw_empty = fifo_data_to_sw_empty & ~(1<<fifo_idx);
+endfunction : uvm_sw_ipc_push_data
+
+function bit  uvm_sw_ipc::uvm_sw_ipc_pull_data(input int fifo_idx, output [31:0] data);
+	data = fifo_data_push_to_uvm[fifo_idx].pop_front();
+endfunction : uvm_sw_ipc_pull_data
 
 function string uvm_sw_ipc::str_replace(string str, string pattern, string replacement);
   string s;
@@ -156,6 +223,18 @@ function string uvm_sw_ipc::str_format_one_arg(input string str, bit [31:0] arg,
   end
   return str;
 endfunction
+
+
+task uvm_sw_ipc::print_info(bit [31:0] cmd);
+	string str = vif.backdoor_get_string(fifo_data_to_uvm[0].pop_front());
+	string all_str = str_format(str,fifo_data_to_uvm[1]);
+	case(cmd)
+		32'h6: `uvm_info("sw info",all_str,UVM_LOW)
+		32'h7: `uvm_warning("sw_info",all_str)
+		32'h8: `uvm_error("sw_info",all_str)
+		32'h9: `uvm_fatal("sw_info",all_str)
+	endcase
+endtask 
 
 
 `endif // UVM_SW_IPC_SV
--- riscv/tests/uvm_sw_ipc/uvm_sw_ipc.c
+++ riscv/tests/uvm_sw_ipc/uvm_sw_ipc.c
@@ -1,11 +1,104 @@
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "uvm_sw_ipc.h"
 
 
 static volatile spy_st uvm_sw_ipc __attribute__ ((section (".uvm_sw_ipc")));
 
-
 // TODO: implement uvm_sw_ipc_* functions
+void uvm_sw_ipc_quit(void){
+	uvm_sw_ipc.cmd = 1;
+}
+
+void uvm_sw_ipc_print_info(uint32_t arg_cnt, char const *const str,  ...){
+	int data_idx = 1;
+
+	va_list argp;
+	va_start (argp,str);
+
+	uvm_sw_ipc.fifo_data_to_uvm[0]= str;
+
+	while(arg_cnt > 0){
+		uint32_t tmp = (uint32_t) va_arg(argp,int);
+		uvm_sw_ipc.fifo_data_to_uvm[1] = tmp;
+		arg_cnt--;
+	}
+	uvm_sw_ipc.cmd = 6;
+}
+void uvm_sw_ipc_gen_event(uint32_t event_idx){
+	uvm_sw_ipc.fifo_data_to_uvm[0] = event_idx;
+	uvm_sw_ipc.cmd = 2;
+}
+void uvm_sw_ipc_wait_event(uint32_t event_idx){
+	uvm_sw_ipc_gen_event(event_idx + 1024);
+	while(uvm_sw_ipc.cmd != 3 || uvm_sw_ipc.fifo_data_to_sw[0]!=event_idx);
+}
+
+void uvm_sw_ipc_push_data(uint32_t fifo_idx, uint32_t data){
+	uvm_sw_ipc.fifo_data_to_uvm[0] = fifo_idx;
+	uvm_sw_ipc.fifo_data_to_uvm[fifo_idx] = data;
+	uvm_sw_ipc.cmd = 4;
+}
+
+bool uvm_sw_ipc_pull_data(uint32_t fifo_idx, uint32_t *data){
+	if((uvm_sw_ipc.fifo_data_to_sw_empty<<(31-fifo_idx))>>31 == 0){
+		uvm_sw_ipc.fifo_data_to_uvm[0] = fifo_idx;
+		uvm_sw_ipc.cmd = 5;
+		*data = uvm_sw_ipc.fifo_data_to_sw[fifo_idx];
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+void uvm_sw_ipc_print_warning(uint32_t arg_cnt, char const *const str,  ...){
+	int data_idx = 1;
+
+	va_list argp;
+	va_start (argp,str);
+
+	uvm_sw_ipc.fifo_data_to_uvm[0]= str;
+
+	while(arg_cnt > 0){
+		uint32_t tmp = (uint32_t) va_arg(argp,int);
+		uvm_sw_ipc.fifo_data_to_uvm[1] = tmp;
+		arg_cnt--;
+	}
+	uvm_sw_ipc.cmd = 7;
+}
+
+void uvm_sw_ipc_print_error(uint32_t arg_cnt, char const *const str,  ...){
+	int data_idx = 1;
+
+	va_list argp;
+	va_start (argp,str);
+
+	uvm_sw_ipc.fifo_data_to_uvm[0]= str;
+
+	while(arg_cnt > 0){
+		uint32_t tmp = (uint32_t) va_arg(argp,int);
+		uvm_sw_ipc.fifo_data_to_uvm[1] = tmp;
+		arg_cnt--;
+	}
+	uvm_sw_ipc.cmd = 8;
+}
+
+void uvm_sw_ipc_print_fatal(uint32_t arg_cnt, char const *const str,  ...){
+	int data_idx = 1;
+
+	va_list argp;
+	va_start (argp,str);
+
+	uvm_sw_ipc.fifo_data_to_uvm[0]= str;
+
+	while(arg_cnt > 0){
+		uint32_t tmp = (uint32_t) va_arg(argp,int);
+		uvm_sw_ipc.fifo_data_to_uvm[1] = tmp;
+		arg_cnt--;
+	}
+	uvm_sw_ipc.cmd = 9;
+}
+
